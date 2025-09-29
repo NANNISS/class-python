4f3c1e99ed7691f376b13255470c07f6395b9f42
alanis <alaniscroce25@gmail.com>
2025-09-29 18:06:01 -0300

    se agrega readme

---
+++ inicio.py
@@ -0,0 +1,23 @@
+print("hola mundo")
+
+entero=10
+flotante=10.5
+cadena="hola"
+booleano=True
+
+print("entero",entero)
+print(type(entero))
+
+print("flotante",flotante)
+print(type(flotante))
+
+print("cadena",cadena)
+print(type(cadena))
+
+print("booleano",booleano)
+print(type(booleano))
+
+numero1=10
+numero2=20
+#suma=numero1+numero2
+print("suma: ", numero1+numero2)
